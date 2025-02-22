39133481c1be4a13ec2ab6ca972ae575146acd14
Tjjvdo <143800551+Tjjvdo@users.noreply.github.com>
2025-02-22 15:23:26 +0100

    update

---
+++ Javascript/WeatherAPI.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const actueelweerDiv = document.getElementById('actueel-weer');
+    const weersinvloedenDiv = document.getElementById('weersinvloeden');
+    const machineStatusDiv = document.getElementById('machine-status');
+
+});
