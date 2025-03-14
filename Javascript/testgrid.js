ea5674fc6827f8cf8c190c60ea3a1aac1e2b3051
Tjjvdo <tjj.vandenouweland@student.avans.nl>
2025-03-14 11:56:59 +0100

    update

--- Javascript/testgrid.js
+++ Javascript/testgrid.js
@@ -31,7 +31,7 @@ class TestGrid {
             const ingredientId = event.dataTransfer.getData('text/plain');
 
             if (ingredientId) {
-                const ingredientElementOrigineel = gemengdeIngredientenLijst.querySelector(`.ingrediënt[data-ingredient-id="${ingredientId}"]`);
+                const ingredientElementOrigineel = document.querySelector(`.ingrediënt[data-ingredient-id="${ingredientId}"]`);
 
                 if (ingredientElementOrigineel) {
                     if (vierkantje.children.length > 0) {
@@ -39,6 +39,7 @@ class TestGrid {
                     }
 
                     const ingredientElementKloon = ingredientElementOrigineel.cloneNode(true);
+                    ingredientElementKloon.style.boxSizing = "";
                     vierkantje.appendChild(ingredientElementKloon);
 
                     ingredientElementKloon.draggable = false;
